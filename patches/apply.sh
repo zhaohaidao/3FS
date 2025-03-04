33da06420fc66660f7e418990cfcada922f3948c
SF-Zhou <sfzhou.scut@gmail.com>
2025-03-04 21:10:07 +0800

    Add rocksdb patch to solve symbol conflict issues (#88)

--- patches/apply.sh
+++ patches/apply.sh
@@ -10,6 +10,12 @@ else
     git -C ../third_party/rocksdb apply ../../patches/rocksdb.patch
 fi
 
+if git -C ../third_party/rocksdb apply --reverse --check ../../patches/rocksdb.patch.2 &>/dev/null; then
+    echo "rocksdb patch 2 already applied. skipping."
+else
+    git -C ../third_party/rocksdb apply ../../patches/rocksdb.patch.2
+fi
+
 if git -C ../third_party/folly apply --reverse --check ../../patches/folly.patch &>/dev/null; then
     echo "folly patch already applied. skipping."
 else
